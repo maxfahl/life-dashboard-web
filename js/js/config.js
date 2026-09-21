1c5233ec033c20f28d8705936c311e8569dd2cde
Max Fahl <max@fahl.se>
2026-09-21 05:36:31 -0400

    Add Supabase configuration constants

---
+++ js/js/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL='https://kvbzadmmbqzlvzzcutwc.supabase.co';
+export const SUPABASE_KEY='sb_publishable_rHVonLfrbUkrn5VcD77paA_Raoscwti';
+export const APP_URL='https://maxfahl.github.io/life-dashboard-web/';
